74707aecfc956a02f249ec9c41a7dbe6204340ef
wwalker2 <wwalker2@xula.edu>
2016-10-26 14:37:02 -0500

    Commit 1

---
+++ NCF 5th Floor Game/NCF5thFloorGame/Source/NCF5thFloorGame/NCF5thFloorGameGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "NCF5thFloorGame.h"
+#include "NCF5thFloorGameGameMode.h"
+
+
+
+
---
+++ NCF 5th Floor Game/NCF5thFloorGame/Source/NCF5thFloorGame/NCF5thFloorGameGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "NCF5thFloorGameGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NCF5THFLOORGAME_API ANCF5thFloorGameGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
