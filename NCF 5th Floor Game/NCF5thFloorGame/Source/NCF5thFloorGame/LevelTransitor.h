01ec5d218b94f9293c33ad416570958923aca689
wwalker2 <wwalker2@xula.edu>
2016-11-16 09:44:05 -0600

    Commit 4

---
+++ NCF 5th Floor Game/NCF5thFloorGame/Source/NCF5thFloorGame/LevelTransitor.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "LevelTransitor.generated.h"
+
+UCLASS()
+class NCF5THFLOORGAME_API ALevelTransitor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALevelTransitor();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	UPROPERTY(EditAnywhere, Category = "Level")
+		ULevel* NewLevel;
+	
+};
